4103c642d1ef4f0492b0823e58ec3fcd19466bbf
Lincoln <lincolnmorais@icloud.com>
2024-05-21 19:21:11 +0200

    small adjustments

--- start.prod.sh
+++ start.prod.sh
@@ -8,6 +8,6 @@ echo "Building Docker images..."
 docker compose -f docker-compose.yml build || { echo "Build failed"; exit 1; }
 
 echo "Starting Docker containers..."
-docker compose -f docker-compose.yml up -d --scale api=3 --remove-orphans || { echo "Failed to start containers"; exit 1; }
+docker compose -f docker-compose.yml up -d --remove-orphans || { echo "Failed to start containers"; exit 1; }
 
 echo "Containers started successfully."
